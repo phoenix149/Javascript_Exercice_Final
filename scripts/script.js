5c7eee74f58f6f9050fb71e2bc7793a9ceb2b6f6
Anthony Carraud <a.carraud@gmail.com>
2024-11-08 14:28:37 +0100

    Ajout des dossiers et fichiers

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const personnages = [ 
+    { nom: "Luke Skywalker", espece: "Humain", affiliation: "Jedi" }, 
+    { nom: "Darth Vader", espece: "Humain", affiliation: "Sith" }, 
+    { nom: "Yoda", espece: "Yoda's species", affiliation: "Jedi" }, 
+    { nom: "Obi-Wan Kenobi", espece: "Humain", affiliation: "Jedi" } 
+    ];
+
+    document.write("Syndrome de la page blanche je vois pas comment faire mais j'abandonne pas et je mettrais l'exercice sur mon Github." + '<br>' + '<br>');
+    document.write("En attendant c'est parti pour coder en voiture en direction de la charente-maritime."+'<br>' + '<br>');
+
+    document.write('<br>'+'<a href="https://github.com/phoenix149/Javascript_Exercice_Final">Exercice sur mon Github</a>');
+    
